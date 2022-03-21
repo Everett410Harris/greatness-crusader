15c7b1f2b858cfbbaa53f260d9081aaa47ae6379
Everett Harris <everettharr979@gmail.com>
2022-03-21 19:17:17 -0400

    changed form color, made buttons

--- GreatCrusCalculator1/GreatCrusCalculator1/Form1.Designer.cs
+++ GreatCrusCalculator1/GreatCrusCalculator1/Form1.Designer.cs
@@ -28,22 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbl_Display = new System.Windows.Forms.TextBox();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.btnDivide, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnEqual, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btn0, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnDot, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnMultiply, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnSubtract, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnAddition, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 176);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(364, 350);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lbl_Display
+            // 
+            this.lbl_Display.Font = new System.Drawing.Font("Felix Titling", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Display.Location = new System.Drawing.Point(12, 12);
+            this.lbl_Display.Multiline = true;
+            this.lbl_Display.Name = "lbl_Display";
+            this.lbl_Display.Size = new System.Drawing.Size(358, 42);
+            this.lbl_Display.TabIndex = 1;
+            this.lbl_Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.lbl_Display.WordWrap = false;
+            // 
+            // btn7
+            // 
+            this.btn7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn7.ForeColor = System.Drawing.Color.Red;
+            this.btn7.Location = new System.Drawing.Point(3, 3);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(85, 81);
+            this.btn7.TabIndex = 1;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn8.ForeColor = System.Drawing.Color.Red;
+            this.btn8.Location = new System.Drawing.Point(94, 3);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(85, 81);
+            this.btn8.TabIndex = 2;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn9.ForeColor = System.Drawing.Color.Red;
+            this.btn9.Location = new System.Drawing.Point(185, 3);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(85, 81);
+            this.btn9.TabIndex = 3;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnAddition.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAddition.Font = new System.Drawing.Font("Bernard MT Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAddition.ForeColor = System.Drawing.Color.Yellow;
+            this.btnAddition.Location = new System.Drawing.Point(276, 3);
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.Size = new System.Drawing.Size(85, 81);
+            this.btnAddition.TabIndex = 4;
+            this.btnAddition.Text = "+";
+            this.btnAddition.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            this.btn4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn4.ForeColor = System.Drawing.Color.Red;
+            this.btn4.Location = new System.Drawing.Point(3, 90);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(85, 81);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn5.ForeColor = System.Drawing.Color.Red;
+            this.btn5.Location = new System.Drawing.Point(94, 90);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(85, 81);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn6.ForeColor = System.Drawing.Color.Red;
+            this.btn6.Location = new System.Drawing.Point(185, 90);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(85, 81);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnSubtract.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnSubtract.Font = new System.Drawing.Font("Broadway", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSubtract.ForeColor = System.Drawing.Color.Yellow;
+            this.btnSubtract.Location = new System.Drawing.Point(276, 90);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(85, 81);
+            this.btnSubtract.TabIndex = 8;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn3.ForeColor = System.Drawing.Color.Red;
+            this.btn3.Location = new System.Drawing.Point(3, 177);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(85, 81);
+            this.btn3.TabIndex = 9;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn2.ForeColor = System.Drawing.Color.Red;
+            this.btn2.Location = new System.Drawing.Point(94, 177);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(85, 81);
+            this.btn2.TabIndex = 10;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.ForeColor = System.Drawing.Color.Red;
+            this.button9.Location = new System.Drawing.Point(185, 177);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(85, 81);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "1";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnMultiply.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnMultiply.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMultiply.ForeColor = System.Drawing.Color.Yellow;
+            this.btnMultiply.Location = new System.Drawing.Point(276, 177);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(85, 81);
+            this.btnMultiply.TabIndex = 12;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = false;
+            // 
+            // btnDot
+            // 
+            this.btnDot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDot.ForeColor = System.Drawing.Color.Red;
+            this.btnDot.Location = new System.Drawing.Point(3, 264);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(85, 83);
+            this.btnDot.TabIndex = 13;
+            this.btnDot.Text = ".";
+            this.btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn0.ForeColor = System.Drawing.Color.Red;
+            this.btn0.Location = new System.Drawing.Point(94, 264);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(85, 83);
+            this.btn0.TabIndex = 14;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEqual.ForeColor = System.Drawing.Color.Red;
+            this.btnEqual.Location = new System.Drawing.Point(185, 264);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(85, 83);
+            this.btnEqual.TabIndex = 15;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnDivide.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDivide.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDivide.ForeColor = System.Drawing.Color.Yellow;
+            this.btnDivide.Location = new System.Drawing.Point(276, 264);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(85, 83);
+            this.btnDivide.TabIndex = 16;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DimGray;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
             this.ClientSize = new System.Drawing.Size(382, 538);
+            this.Controls.Add(this.lbl_Display);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button btn7;
+        private TextBox lbl_Display;
+        private Button btn8;
+        private Button btnDivide;
+        private Button btnEqual;
+        private Button btn0;
+        private Button btnDot;
+        private Button btnMultiply;
+        private Button button9;
+        private Button btn2;
+        private Button btn3;
+        private Button btnSubtract;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btnAddition;
+        private Button btn9;
     }
 }

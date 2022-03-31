ad7bfa1f56a273dac3070c3c5abbe56085696aea
Everett Harris <everettharr979@gmail.com>
2022-03-30 23:09:54 -0400

    working on datetime

--- GreatCrusCalculator1/GreatCrusCalculator1/DaysUntil.Designer.cs
+++ GreatCrusCalculator1/GreatCrusCalculator1/DaysUntil.Designer.cs
@@ -29,18 +29,70 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textDaysU = new System.Windows.Forms.TextBox();
+            this.textTotal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textToday = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(124, 77);
+            this.label1.Location = new System.Drawing.Point(26, 202);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(171, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Days Until:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textDaysU
+            // 
+            this.textDaysU.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textDaysU.Location = new System.Drawing.Point(302, 199);
+            this.textDaysU.Name = "textDaysU";
+            this.textDaysU.Size = new System.Drawing.Size(100, 34);
+            this.textDaysU.TabIndex = 1;
+            // 
+            // textTotal
+            // 
+            this.textTotal.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textTotal.Location = new System.Drawing.Point(302, 310);
+            this.textTotal.Name = "textTotal";
+            this.textTotal.Size = new System.Drawing.Size(100, 34);
+            this.textTotal.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(26, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Total Days:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 19F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(26, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Today\'s Date:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textToday
+            // 
+            this.textToday.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textToday.Location = new System.Drawing.Point(302, 82);
+            this.textToday.Name = "textToday";
+            this.textToday.ReadOnly = true;
+            this.textToday.Size = new System.Drawing.Size(100, 34);
+            this.textToday.TabIndex = 5;
             // 
             // DaysUntil
             // 
@@ -48,9 +100,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Sienna;
             this.ClientSize = new System.Drawing.Size(507, 450);
+            this.Controls.Add(this.textToday);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textTotal);
+            this.Controls.Add(this.textDaysU);
             this.Controls.Add(this.label1);
             this.Name = "DaysUntil";
             this.Text = "DaysUntil";
+            this.Load += new System.EventHandler(this.DaysUntil_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +117,10 @@
         #endregion
 
         private Label label1;
+        private TextBox textDaysU;
+        private TextBox textTotal;
+        private Label label2;
+        private Label label3;
+        private TextBox textToday;
     }
 }
--- GreatCrusCalculator1/GreatCrusCalculator1/DaysUntil.cs
+++ GreatCrusCalculator1/GreatCrusCalculator1/DaysUntil.cs
@@ -12,9 +12,62 @@ namespace GreatCrusCalculator1
 {
     public partial class DaysUntil : Form
     {
+        private static readonly object DaysU;
+
         public DaysUntil()
         {
             InitializeComponent();
         }
+
+        private void DaysUntil_Load(object sender, EventArgs e)
+        {
+            textToday.Text = DateTime.Now.ToString();
+            textTotal.Text = 
+
+
+        }
+
+        public static void Dates()
+        {
+
+            DateTime firstDate = DateTime.Now;
+            DateTime secondDate = TextBox.DaysU;
+
+            TimeSpan diff = firstDate - secondDate;
+
+            Console.WriteLine("Days : " + diff.TotalDays.ToString());
+            Console.WriteLine("Hours : " + diff.TotalHours.ToString());
+            Console.WriteLine("Minutes : " + diff.TotalMinutes.ToString());
+
+
+        }
+
+        //public static void DateTimeDiff(DateTime dateTime,  DateTime DaysU, out int years, out int months, out int days)
+        //{
+        //    DateTime start = DateTime.Now;
+        //    DateTime end = DaysU > DateTime.Now ? DaysU : DateTime.Now;
+
+        //    years = 0;
+        //    months = 0;
+        //    days = 0;
+
+        //    while (start.AddYears(1) < end)
+        //    {
+        //        start = start.AddYears(1);
+        //        years++;
+        //    }
+
+        //    while (start.AddMonths(1) < end)
+        //    {
+        //        start = start.AddMonths(1);
+        //        months++;
+        //    }
+
+        //    while (start.AddDays(1) < end)
+        //    {
+        //        start = start.AddDays(1);
+        //        days++;
+        //    }
+        //}
     }
 }
